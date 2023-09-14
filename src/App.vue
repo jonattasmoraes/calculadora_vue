a16fc9c1c7f75958b53ea87b67addd1bad2a6f86
Jonattas moraes <jonattasmoraes@hotmail.com>
2023-09-14 20:40:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+
+    <div>
+      <label for="numero1">Número 1:</label>
+      <input type="number" v-model="numero1" @input="validarNumero1" />
+    </div>
+
+    <div>
+      <label for="numero2">Número 2:</label>
+      <input type="number" v-model="numero2" @input="validarNumero2" />
+    </div>
+
+    <div>
+      <label for="operacao">Operação:</label>
+      <select v-model="operacao" @change="calcularResultado">
+        <option value="soma">Soma (+)</option>
+        <option value="subtracao">Subtração (-)</option>
+        <option value="multiplicacao">Multiplicação (*)</option>
+        <option value="divisao">Divisão (/)</option>
+      </select>
+    </div>
+
+    <div>
+      <h2>Resultado: {{ resultado }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero1: 1,
+      numero2: 1,
+      operacao: 'soma',
+      resultado: 0,
+    };
+  },
+  methods: {
+    validarNumero1() {
+      if (this.numero1 < 1) {
+        this.numero1 = 1;
+      }
+      this.calcularResultado();
+    },
+    validarNumero2() {
+      if (this.numero2 < 1) {
+        this.numero2 = 1;
+      }
+      this.calcularResultado();
+    },
+    calcularResultado() {
+      switch (this.operacao) {
+        case 'soma':
+          this.resultado = this.numero1 + this.numero2;
+          break;
+        case 'subtracao':
+          this.resultado = this.numero1 - this.numero2;
+          break;
+        case 'multiplicacao':
+          this.resultado = this.numero1 * this.numero2;
+          break;
+        case 'divisao':
+          this.resultado = this.numero1 / this.numero2;
+          break;
+        default:
+          this.resultado = 0;
+      }
+    },
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+label {
+  font-weight: bold;
+}
+
+input {
+  width: 100px;
+  padding: 5px;
+  margin: 5px;
+}
+
+select {
+  padding: 5px;
+  margin: 5px;
+}
+
+h2 {
+  margin-top: 20px;
+}
+</style>
